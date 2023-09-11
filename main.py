30c1a130ed0c3022eae6e6c8843168b3b131d0c6
jdwd40 <jd@jdwd40.com>
2023-09-11 19:27:08 +0100

    add, delete, and list tasks features added

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-# Time Tracker CLI App Documentation Overview
-#
-# The Time Tracker is a command-line application developed in Python. Upon launch, it displays a menu with several options for task management and reporting:
-#
-# - **A**: Add a new task along with its duration.
-# - **D**: Delete an existing task.
-# - **T**: Display all tasks and their durations for today.
-# - **W**: List all tasks and durations for the past 7 days.
-# - **M**: List all tasks and durations for the past 30 days.
-# - **E**: List all tasks and durations stored in the log file.
-#
-# The user can select an option by typing its corresponding letter. For adding tasks, the application prompts for the task name and its duration. All task-related data is stored in a # log file, which the application reads from to display task lists as per the user's request.
-
-# 1. Initialize the Log File
-# First, check if the log file exists. If not, create a new CSV file and write the header.
-
 import datetime
 import csv
+import string
+from collections import defaultdict
 
 # Initialize the log file if it doesn't exist
 try:
@@ -26,125 +12,176 @@ except FileNotFoundError:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-def add_task():
-    ## promt the user for the task name and duration
-    task_name = input("Enter the task name: ").strip()
-    duration = input("Enter the duration (in minutes): ").strip()
+import string
+import datetime
+import csv
 
-    ## check for empty input
+def add_task():
+    task_name = input("\033[97mEnter the task name: \033[0m").strip().translate(str.maketrans('', '', string.digits))
+    duration = input("\033[97mEnter the duration (in minutes): \033[0m").strip()
+
+    # Check for empty input
     if not task_name or not duration:
-        print("Invalid input. Task not added.")
+        print("\033[91mInvalid input. Task not added.\033[0m")
         return
-    
-    ## check for non-numeric input
+
+    # Check for non-numeric input
     if not duration.isnumeric():
-        print("Invalid input. Task not added.")
+        print("\033[91mInvalid input. Task not added.\033[0m")
         return
-    
-    ## add the task to the log file
+
+    # Check for the existing task name
+    existing_task = False
+    with open("timetracker.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            if task_name.lower() == row[1].lower():
+                existing_task = True
+                break
+
+    if existing_task:
+        confirm_add = input("\033[97mAdd duration to existing task? (y/n): \033[0m").lower()
+        if confirm_add != 'y':
+            print("\033[91mTask not added.\033[0m")
+            return
+    else:
+        confirm_new = input("\033[97mCreate a new task for today? (y/n): \033[0m").lower()
+        if confirm_new != 'y':
+            print("\033[91mTask not added.\033[0m")
+            return
+
+    # Add the task to the log file
     date = datetime.datetime.now().strftime("%Y-%m-%d")
     with open("timetracker.csv", "a", newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([date, task_name, duration])
 
-    ## provide feedback to the user
-    print(f"Task '{task_name}' added successfully.")
+    print(f"\033[92mTask '{task_name}' added successfully.\033[0m")
+    list_tasks(1)  # Assuming list_tasks is defined to list today's tasks
 
-# show tasks for today
-    list_tasks(1) 
 
 def delete_task():
-    task_to_delete = input("Enter the name of the task to delete: ").strip()
+    task_to_delete = input("\033[97mEnter the name of the task to delete: \033[0m").strip()
     rows = []
-    task_found = False  # Flag to check if the task exists
+    task_found = False
 
-    # Read the log file and store all rows except the one to be deleted
     with open("timetracker.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
-        fields = next(reader)  # Skip the header
+        fields = next(reader)
         for row in reader:
-            # Skip any rows that don't contain the expected number of elements
             if len(row) != 3:
                 continue
 
             if row[1] == task_to_delete:
-                task_found = True  # Task exists
+                task_found = True
             else:
                 rows.append(row)
-                
-    # Write the updated rows back to the log file
+
     with open("timetracker.csv", "w", newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(fields)
         writer.writerows(rows)
 
-    # Provide user feedback
     if task_found:
-        print("Task deleted successfully.")
-        print("Here are your tasks for today:")
-        list_tasks(0)  # List tasks for today
+        confirm_delete = input("\033[97mTask found. Delete? (y/n): \033[0m").lower()
+        if confirm_delete != 'y':
+            print(f"\033[91mTask '{task_to_delete}' not deleted. Returning to main menu.\033[0m")
+            return
+        print("\033[92mTask deleted successfully.\033[0m")
+        list_tasks(0)
     else:
-        print(f"Task '{task_to_delete}' not found. Returning to main menu.")
+        print(f"\033[91mTask '{task_to_delete}' not found. Returning to main menu.\033[0m")
 
-
-from collections import defaultdict
-
-# Task 3.3: List Tasks
 def list_tasks(days):
     today = datetime.datetime.now().date()
-    task_dict = defaultdict(int)  # A dictionary to store the total duration for each task
-    
-    # Read the log file and populate the task_dict
+    task_dict = defaultdict(int)
+    tasks_found = False 
+
     with open("timetracker.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)  # Skip the header
+        next(reader)
         for row in reader:
-            # Skip any rows that don't contain the expected number of elements
             if len(row) != 3:
                 continue
             task_date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
             delta = today - task_date
-            
+
             if delta.days <= days:
-                task_key = f"{row[1]} ({row[0]})"  # Create a key like "task_name (date)"
-                task_dict[task_key] += int(row[2])  # Sum up the total duration for each task
-                
-    # Print the tasks in a cleaner format
-    print(f"{'Task Name':<30} {'Total Duration (minutes)':<25} {'Date':<10}")
-    print("="*65)
+                task_key = f"{row[1]} ({row[0]})"
+                task_dict[task_key] += int(row[2])
+                tasks_found = True
+
+    print(f"\033[97m{'='*65}\033[0m")
+    if days in [0, 7, 30, 999999]:
+        time_frames = {0: "Today's", 7: "past 7 days'", 30: "past 30 days'", 999999: "All"}
+        print(f"\033[92m{time_frames[days]} Tasks:\033[0m")
+
+    print(f"\033[97m{'Task Name':<30} {'Total Duration (minutes)':<25} {'Date':<10}\033[0m")
+    print(f"\033[97m{'='*65}\033[0m")
+
     for task_key, total_duration in task_dict.items():
         task_name, date = task_key.split(" (")
         date = date.rstrip(")")
-        print(f"{task_name:<30} {total_duration:<25} {date:<10}")
+        print(f"\033[92m{task_name:<30} {total_duration:<25} {date:<10}\033[0m")
+    
+    return tasks_found
+
+def list_entries_for_task():
+    task_to_list = input("\033[97mEnter the name of the task to list (or 'all' for all tasks): \033[0m").strip()
+    task_found = False
+
+    with open("timetracker.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        fields = next(reader)
+        
+        print(f"\033[97m{'='*65}\033[0m")
+        
+        if task_to_list.lower() != 'all':
+            print(f"\033[92mEntries for Task '{task_to_list}':\033[0m")
+        else:
+            print(f"\033[92mAll Task Entries:\033[0m")
+        
+        print(f"\033[97m{'Date':<10} {'Task':<25} {'Duration (minutes)':<25}\033[0m")
+        print(f"\033[97m{'='*65}\033[0m")
+        
+        for row in reader:
+            if len(row) != 3:
+                continue
+            
+            if task_to_list.lower() == 'all' or row[1] == task_to_list:
+                print(f"\033[92m{row[0]:<10} {row[1]:<25} {row[2]:<25}\033[0m")
+                task_found = True
+
+    if not task_found:
+        print(f"\033[91mTask '{task_to_list}' not found. Returning to main menu.\033[0m")
 
 
 def main():
+    print(f"\033[97m{'='*65}\033[0m")
+    print("\033[92mWelcome to JDTT Time Tracker CLI App!\033[0m")
+    # List tasks for today
+    print("\033[97mListing tasks for today...\033[0m")
+    if not list_tasks(0):  # Assuming list_tasks returns False when no tasks are found for today
+        print("\033[93mNo tasks for today yet.\033[0m")
+
     while True:
-        # Display the menu options on a single line
-        print("Menu: A-Add | D-Delete | T-Today | W-7 Days | M-30 Days | E-All | Q-Quit")
-        
-        # Get user choice
-        choice = input("Choice: ").upper()
-        
+        print("\033[97mMenu: A-Add | D-Delete | T-Today | W-7 Days | M-30 Days | E-All | L-List Task | Q-Quit\033[0m")
+        choice = input("\033[97mChoice: \033[0m").upper()
+
         if choice == "A":
             add_task()
         elif choice == "D":
             delete_task()
-        elif choice == "T":
-            list_tasks(0)
-        elif choice == "W":
-            list_tasks(7)
-        elif choice == "M":
-            list_tasks(30)
-        elif choice == "E":
-            list_tasks(999999)  # List all tasks
+        elif choice in ["T", "W", "M", "E"]:
+            list_tasks({"T": 0, "W": 7, "M": 30, "E": 999999}[choice])
+        elif choice == "L":
+            list_entries_for_task()
         elif choice == "Q":
             break
         else:
-            list_tasks(1)  # List tasks for today
+            list_tasks(0)
 
 if __name__ == "__main__":
     main()
-
-
 
